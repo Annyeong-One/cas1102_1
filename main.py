7c18285e0f60ed160d66e483e1949387b8a1ef72
Annyeong-One <me@annyeong.one>
2025-09-11 17:16:11 +0900

    week 2 in-class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello')
+class demo:
+    def __new__(cls, *args, **kwargs):
+        print("__new__")
+        obj = super().__new__(cls)
+        return obj
+    def __init__(self, value):
+        print("__init__")
+        self.value = value
+
+d=demo(42)
+print(d.value)
---
+++ wk2_inclass/ex2.py
@@ -0,0 +1,29 @@
+class Temperature:
+    unit = "Celsius"
+
+    def __init__(self, value):
+        self.value = value
+
+    # TODO: Instance method
+    def display(self):
+        print(f"{self.value} {self.unit}")
+
+    # TODO: Class method with relevant decorator
+    @classmethod
+    def change_unit(cls, new_unit):
+        cls.unit = new_unit
+
+    # TODO: Static method with relevant decorator
+    @staticmethod
+    def to_fahrenheit(celsius):
+        return celsius * 1.8 + 32
+
+
+t1 = Temperature(100)
+t1.display()
+print(Temperature.to_fahrenheit(100))
+Temperature.change_unit("Kelvin")
+t1.display()
+# unit is a class attribute, we changed it at line 26,
+# and therefore upon calling display() afterwards,
+# the printed (displayed) unit is "Kelvin"
