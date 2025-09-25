a165c9c042063238bd9fc7a7a5e22a698824f7a6
윤지호 / Jeeho Yoon <79385636+Annyeong-One@users.noreply.github.com>
2025-09-25 17:39:15 +0900

    Add files via upload

---
+++ wk4_inclass/helloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    //std::cout << "Hello World!\n";
+    std::cout<<"Jeeho Yoon"<<std::endl<<12+345<<std::endl;
+}
---
+++ wk4_inclass/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "math.h"
+
+int main() {
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << add(x, y) << std::endl;
+}
